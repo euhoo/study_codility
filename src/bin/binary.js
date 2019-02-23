7675236e3c5e4fc0eadffeac04b85c8b934fc5c9
euhoo <euhoo@mail.ru>
2019-02-23 16:21:32 +0300

    let->const in binary

--- src/bin/binary.js
+++ src/bin/binary.js
@@ -1,15 +1,18 @@
 export default (num) => {
   const binNum = num.toString(2); // переводим в двоичное представление и строку
-  let max = 0;
-  let current = 0;
+  const obj = {
+    current: 0,
+    max: 0,
+  };
   for (let i = 0; i < binNum.length; i += 1) {
-    if (binNum[i] === '0') current += 1;
-    if (binNum[i] === '1') {
-      if (current > max) {
-        max = current;
-        current = 0;
+    const value = binNum[i];
+    if (value === '0') obj.current += 1;
+    if (value === '1') {
+      if (obj.current > obj.max) {
+        obj.max = obj.current;
+        obj.current = 0;
       }
     }
   }
-  return max;
+  return obj.max;
 };
